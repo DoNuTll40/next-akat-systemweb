fea6439ad200942e541eb100ed3951fa46e9256e
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2025-04-05 20:13:27 +0700

    update code footer and add popover version detail.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 "use client"
 
+import axios from "@/configs/axios.mjs"
+import { Popover } from "antd"
+import { BookUser } from "lucide-react"
+import { Dialog } from "primereact/dialog"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
 
@@ -24,7 +28,6 @@ export default function Footer() {
       if (rs.status === 200) {
         setAllVersionDetail(rs.data.data);
         setVersionDetail(latestVersion);
-        console.log(allVersionDetail)
       }
     } catch (err) {
       toast.error(err.response?.data?.message);
@@ -36,11 +39,25 @@ export default function Footer() {
     return new Date(current.created_at) > new Date(latest.created_at) ? current : latest;
   }, allVersionDetail[0] || null);
 
+  const content = (
+    <ul>
+      <li>{latestVersion?.api_version_detail_comment}</li>
+    </ul>
+  );
+
+  const title = (
+    <div className="flex gap-1 items-center text-md">
+      <BookUser size={16} />
+      <p>รายละเอียด</p>
+    </div>
+  )
+
   return (
-    <div className="bg-white hidden w-full gap-2 md:flex justify-center items-center text-xs min-h-8 border-t border-gray-200">
-      <p className="line-clamp-1">&copy; Copyright 2025 โรงพยาบาลอากาศอำนวย</p>
-      {JSON.stringify(versionDetail)}
-      <p>v 0.0.1</p>
+    <div className="bg-white hidden w-full gap-2 md:flex justify-center items-center text-xs min-h-8 border-t border-gray-200 line-clamp-1">
+      <p>&copy; Copyright 2025 โรงพยาบาลอากาศอำนวย</p>
+      <Popover content={content} title={title}>
+        <p>Version {latestVersion?.api_versions?.api_version_name}</p>
+      </Popover>
     </div>
   )
 }
