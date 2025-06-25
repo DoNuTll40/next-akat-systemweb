43d59f5e5cda9f37ecb22105e166d6cdf2d4cf64
DoNuTll40 <iinter731@gmail.com>
2025-06-25 09:36:42 +0700

    update code

--- src/components/ButtonBackBefore.jsx
+++ src/components/ButtonBackBefore.jsx
@@ -2,10 +2,27 @@
 
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ButtonBackBefore() {
-
+    const [isAuthen, setIsAuthen] = useState({});
     const router = useRouter();
+
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        if (localStorage.getItem("isAuthen")) {
+          setIsAuthen(JSON.parse(localStorage.getItem("isAuthen")));
+        }
+      }
+    }, [])
+
+    if (typeof window !== 'undefined' && window.history.length > 1) {
+      router.back();
+    } else if (isAuthen?.status) {
+      router.push(`/${isAuthen?.status?.toLowerCase()}`);
+    } else {
+      router.push("/auth/login");
+    }
 
     return (
         <div className=" absolute top-5 left-5">
