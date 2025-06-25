3ad3fc037bd1242ba527377639b772f1bf491fe5
DoNuTll40 <iinter731@gmail.com>
2025-06-25 13:00:18 +0700

    update code

--- src/components/ButtonBackBefore.jsx
+++ src/components/ButtonBackBefore.jsx
@@ -2,7 +2,7 @@
 
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function ButtonBackBefore() {
     const [isAuthen, setIsAuthen] = useState({});
